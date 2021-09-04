c0092dd6a5eec81075a199dba944af607c4c2a1e
Abhinayakonala <abhinayakonala66@gmail.com>
2021-09-04 19:31:41 +0530

    ListDemo

--- 1-Java/01-java-learn/src/com/revature/app/objectclass/AnotherClass.java
+++ 1-Java/01-java-learn/src/com/revature/app/objectclass/AnotherClass.java
@@ -3,7 +3,7 @@ package com.revature.app.objectclass;
 public class AnotherClass {
 
 	public static void main(String[] args) {
-		Person person = new Person("John, 10", 0);
+		Person person = new Person("John, 10");
 		System.out.println(person);
 
 	}
--- 1-Java/01-java-learn/src/com/revature/app/objectclass/Person.java
+++ 1-Java/01-java-learn/src/com/revature/app/objectclass/Person.java
@@ -1,6 +1,6 @@
 package com.revature.app.objectclass;
 
-public class Person {
+public class Person implements Comparable{
 	private String name;
 	private int age;
 	
@@ -9,12 +9,32 @@ public class Person {
 		this.name = name;
 		this.age = age;
 		}
+	
+	public String getName() {
+		return name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+	
 
 	@Override
 	public String toString() {
 		
 		return "Person [name=" + name + ", age=" + age + "]";
 	}
+
+	@Override
+	public int compareTo(Object o) {
+       Person otherPerson = (Person) o;
+       System.out.println(this.name + " " + otherPerson.name);
+	//return name.compareTo(otherPerson.name); //+ve -First object comes first; -ve - 2nd object
+	   return age - otherPerson.age;
+	}
+	
+
+
 }
 
 
