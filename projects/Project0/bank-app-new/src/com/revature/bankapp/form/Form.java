1591ba6c9535293a8eee268bc4fce3ac4a3808aa
Abhinayakonala <abhinayakonala66@gmail.com>
2021-09-09 13:23:04 +0530

    BankApp

---
+++ projects/Project0/bank-app-new/src/com/revature/bankapp/form/Form.java
@@ -0,0 +1,27 @@
+package com.revature.bankapp.form;
+
+public abstract class Form {
+	protected String name;
+	protected boolean success;
+	
+	public Form(String name) {
+		this.name = name;
+		success = false;
+	}
+	private void displayTitle() {
+		System.out.println("***" + name + "***");
+	}
+
+	public void captureDataAndPerformAction() {
+		while (!success) {
+			displayTitle();
+			captureData();
+			action();
+		}
+	}
+	
+	public abstract void captureData();
+	
+	public abstract void action();
+
+}
---
+++ projects/Project0/bank-app-new/src/com/revature/bankapp/menu/CustomerMainMenu.java
@@ -0,0 +1,17 @@
+package com.revature.bankapp.menu;
+
+public class CustomerMainMenu extends Menu {
+	
+	public CustomerMainMenu(String name) {
+		super(name);
+		addMenuItem("View Account");
+		addMenuItem("Create Account");
+	}
+
+	@Override
+	void handleAction() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
